3fd4a507493bb40a54c4838f930460714f1a3923
= <dedeaulad10@gmail.com>
2026-03-14 12:07:05 +0700

    fixing frame

--- src/event/4r/merkico/frames.js
+++ src/event/4r/merkico/frames.js
@@ -10,46 +10,46 @@ export const frames = [
         }, 
         slots: [
             {
-                x: 41, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 45, 
+                y:110, 
+                width : 534, 
+                height : 428
             }, 
             {
-                x: 41, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 45, 
+                y:582, 
+                width : 534, 
+                height : 428
             },
             {
-                x: 41, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 45, 
+                y:1054, 
+                width : 534, 
+                height : 428
             }, 
             {
-                x: 633, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:110, 
+                width : 534, 
+                height : 428
             }, 
             {
-                x: 633, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:582, 
+                width : 534, 
+                height : 428
             },
             {
-                x: 633, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:1054, 
+                width : 534, 
+                height : 428
             }
         ]
     }, 
     {
-        id: 'folklore 4r', 
-        name : 'Folklore', 
+        id: 'dbooth-biru', 
+        name : 'Dbooth Biru', 
         image : '/event/merkico/frames/dbooth-biru.png', 
         photoCount : '6', 
         canvas: {
@@ -58,46 +58,46 @@ export const frames = [
         }, 
         slots: [
             {
-                x: 41, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 43, 
+                y:108, 
+                width : 534, 
+                height : 428
             }, 
             {
-                x: 41, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 43, 
+                y:581, 
+                width : 534, 
+                height : 428
             },
             {
-                x: 41, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 43, 
+                y:1052, 
+                width : 534, 
+                height : 428
             }, 
             {
-                x: 633, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:108, 
+                width : 534, 
+                height : 428
             }, 
             {
-                x: 633, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:581, 
+                width : 534, 
+                height : 428
             },
             {
-                x: 633, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:1052, 
+                width : 534, 
+                height : 428
             }
         ]
     }, 
     {
-        id: 'lover 4r', 
-        name : 'lover', 
+        id: 'ramadhan-kareem', 
+        name : 'Ramadhan Kareem', 
         image : '/event/merkico/frames/ramadhan-kareem.png', 
         photoCount : '6', 
         canvas: {
@@ -106,46 +106,46 @@ export const frames = [
         }, 
         slots: [
             {
-                x: 41, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 43, 
+                y:110, 
+                width : 527, 
+                height : 425
             }, 
             {
-                x: 41, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 43, 
+                y:577, 
+                width : 527, 
+                height : 425
             },
             {
-                x: 41, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 43, 
+                y:1047, 
+                width : 527, 
+                height : 425
             }, 
             {
-                x: 633, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 622, 
+                y:110, 
+                width : 527, 
+                height : 425
             }, 
             {
-                x: 633, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 622, 
+                y:577, 
+                width : 527, 
+                height : 425
             },
             {
-                x: 633, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 622, 
+                y:1047, 
+                width : 527, 
+                height : 425
             }
         ]
     }, 
     {
-        id: 'lover 4r', 
-        name : 'lover', 
+        id: 'eid', 
+        name : 'Eid', 
         image : '/event/merkico/frames/eid.png', 
         photoCount : '6', 
         canvas: {
@@ -154,46 +154,46 @@ export const frames = [
         }, 
         slots: [
             {
-                x: 41, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 45, 
+                y:108, 
+                width : 525, 
+                height : 421
             }, 
             {
-                x: 41, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 45, 
+                y:576, 
+                width : 525, 
+                height : 421
             },
             {
-                x: 41, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 45, 
+                y:1046, 
+                width : 525, 
+                height : 421
             }, 
             {
-                x: 633, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:108, 
+                width : 525, 
+                height : 421
             }, 
             {
-                x: 633, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:576, 
+                width : 525, 
+                height : 421
             },
             {
-                x: 633, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:1046, 
+                width : 525, 
+                height : 421
             }
         ]
     }, 
     {
-        id: 'lover 4r', 
-        name : 'lover', 
+        id: 'cats-are-cute', 
+        name : 'Cats Are Cute', 
         image : '/event/merkico/frames/cats-are-cute.png', 
         photoCount : '6', 
         canvas: {
@@ -202,40 +202,136 @@ export const frames = [
         }, 
         slots: [
             {
-                x: 41, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 45, 
+                y:111, 
+                width : 535, 
+                height : 422
             }, 
             {
-                x: 41, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 45, 
+                y:581, 
+                width : 535, 
+                height : 422
             },
             {
-                x: 41, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 45, 
+                y:1050, 
+                width : 535, 
+                height : 422
             }, 
             {
-                x: 633, 
-                y:152, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:111, 
+                width : 535, 
+                height : 422
             }, 
             {
-                x: 633, 
-                y:630, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:581, 
+                width : 535, 
+                height : 422
             },
             {
-                x: 633, 
-                y:1108, 
-                width : 530, 
-                height : 397
+                x: 625, 
+                y:1050, 
+                width : 535, 
+                height : 422
+            }
+        ]
+    }, 
+    {
+        id: 'lover 4r', 
+        name : 'lover', 
+        image : '/event/merkico/frames/pink.png', 
+        photoCount : '6', 
+        canvas: {
+            width: 1205,
+            height: 1795
+        }, 
+        slots: [
+            {
+                x: 42, 
+                y:135, 
+                width : 548, 
+                height : 415
+            }, 
+            {
+                x: 42, 
+                y:630, 
+                width : 548, 
+                height : 415
+            },
+            {
+                x: 42, 
+                y:1099, 
+                width : 548, 
+                height : 415
+            }, 
+            {
+                x: 623, 
+                y:135, 
+                width : 548, 
+                height : 415
+            }, 
+            {
+                x: 623, 
+                y:630, 
+                width : 548, 
+                height : 415
+            },
+            {
+                x: 623, 
+                y:1099, 
+                width : 548, 
+                height : 415
+            }
+        ]
+    }, 
+    {
+        id: 'recap-dump', 
+        name : 'Recap Dump', 
+        image : '/event/merkico/frames/recap-dump.png', 
+        photoCount : '6', 
+        canvas: {
+            width: 1205,
+            height: 1795
+        }, 
+        slots: [
+            {
+                x: 35, 
+                y:151, 
+                width : 523, 
+                height : 399
+            }, 
+            {
+                x: 35, 
+                y:622, 
+                width : 523, 
+                height : 399
+            },
+            {
+                x: 35, 
+                y:1105, 
+                width : 523, 
+                height : 399
+            }, 
+            {
+                x: 626, 
+                y:151, 
+                width : 523, 
+                height : 399
+            }, 
+            {
+                x: 626, 
+                y:622, 
+                width : 523, 
+                height : 399
+            },
+            {
+                x: 626, 
+                y:1105, 
+                width : 523, 
+                height : 399
             }
         ]
     }, 
